5e8f4370577e05a26c8aa003a8dbddd7ebc780ef
“[firstname <faith.gacheri@student.moringaschool.com>
2022-10-26 13:40:37 +0300

    Assignment Completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
 
+    attr_accessor :knowledge
+
+    def initialize
+         @knowledge = []
+    end
+
+    def learn subject
+      @knowledge << subject
+
+    end
+    # def knowledge
+    #     @knowledge
+
+    # end
 end
